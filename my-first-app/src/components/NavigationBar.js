9f3d1f2b53cdbf623a8ed197d0f39af4c1e63ab2
nikolaivrzhesinsky <15nk33l49@gmail.com>
2023-01-15 17:30:39 +0300

    add comp

---
+++ my-first-app/src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NavigationBar extends React.Component{
+
+    render(){
+        return  ( 
+        <div> NavigationBar</div>
+        );       
+    }
+}
+
+export default NavigationBar;
